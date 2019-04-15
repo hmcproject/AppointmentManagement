40368d9bcb7fff167bf8151b4d447dc9c5855430
Tajwer Jalal <tajwerj@yahoo.com>
2019-04-15 12:47:23 +0500

    Add files via upload

---
+++ Infrastructure/HospitalDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DoctorPatient.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace DoctorPatient.Infrastructure
+{
+    public class HospitalDbContext : IdentityDbContext
+    {
+        public HospitalDbContext() : base("DefaultConnection")
+        {
+            //Empty constructor.
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<DoctorModel> Doctors { get; set; }
+        public DbSet<AppointmentModel> Appointments { get; set; }
+        public DbSet<AdministrationModel> Administrations { get; set; }
+        public DbSet<DepartmentModel> Departments { get; set; }
+        public DbSet<EmployeeModel> Employees { get; set; }
+    }
+}
